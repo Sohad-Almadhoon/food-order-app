d3f37adb428352de06dc6eded4bfc3103cff877f
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2024-06-22 15:42:58 +0300

    ...

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -4,28 +4,28 @@ import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchMeals = async () => {
-      const response = await fetch("");
-      if (response.ok) {
-        throw new Error("Something went Wrong!");
-      }
-      const meals = await response.json();
-      const mealsList = [];
-      meals.map(({ name, description, price }, idx) =>
-        mealsList.push({ idx, name, description, price })
-      );
-      setMeals(mealsList);
-      setIsLoading(false);
-    };
-    fetchMeals().catch((error) => {
-      setIsLoading(false);
-      setError(error.message)
-    });
-  }, []);
+  // const [meals, setMeals] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // useEffect(() => {
+  //   const fetchMeals = async () => {
+  //     const response = await fetch("");
+  //     if (response.ok) {
+  //       throw new Error("Something went Wrong!");
+  //     }
+  //     const meals = await response.json();
+  //     const mealsList = [];
+  //     meals.map(({ name, description, price }, idx) =>
+  //       mealsList.push({ idx, name, description, price })
+  //     );
+  //     setMeals(mealsList);
+  //     setIsLoading(false);
+  //   };
+  //   fetchMeals().catch((error) => {
+  //     setIsLoading(false);
+  //     setError(error.message)
+  //   });
+  // }, []);
   // if (isLoading) return <h1>Loading....</h1>;
   // if (error) return <h1>{error}🧨</h1>;
   return (
